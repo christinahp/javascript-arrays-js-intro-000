de7e1a088307ea8c45db48e504e83774d2c6b742
Christina Petersen <christina.h.petersen4@gmail.com>
2021-03-24 22:52:07 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,34 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+var addElementToBeginningOfArray = (array, element) => {
+  var newArray = [element, ...array]
+  return newArray
+}
+
+var destructivelyAddElementToBeginningOfArray = (array, element) => {
+  array.unshift(element)
+  return array
+}
+
+var addElementToEndOfArray = (array, element) => {
+  var newArray = [...array, element]
+  return newArray
+}
+
+var destructivelyAddElementToEndOfArray = (array, element) => {
+  array.push(element)
+  return array
+}
+
+var accessElementInArray = (array, index) => {
+  return array[index]
+}
+
+var destructivelyRemoveElementFromBeginningOfArray = (array) => {
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
